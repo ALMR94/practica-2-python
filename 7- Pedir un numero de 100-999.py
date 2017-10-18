68fdd8a913761057bbb83b8a344f095a8b2e0b7d
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-18 20:44:39 +0200

    Add files via upload

---
+++ 7- Pedir un numero de 100-999.py
@@ -0,0 +1,7 @@
+# -*- coding: cp1252 -*-
+print "Dime un número de 3 cifras:"
+a= int(raw_input())
+if a<1000 and a>99:
+    print "El número",a,"es válido."
+else:
+    print "El número introducido no es válido o no es de 3 cifras."
