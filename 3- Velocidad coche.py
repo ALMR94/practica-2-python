fda1192dd7ae9e2ba0d25ff71108e655038b0d65
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-18 20:43:34 +0200

    Add files via upload

---
+++ 3- Velocidad coche.py
@@ -0,0 +1,7 @@
+# -*- coding: cp1252 -*-
+print "¿Cuántos km ha recorrido el coche?"
+KM=float(raw_input())
+print "¿En cuántas horas ha hecho dicho recorrido?"
+H=float(raw_input())
+Vel= KM/H
+print "El coche iba a un promedio de", Vel,"KM/H."
