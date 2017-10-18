10d39702cd8239e9a6c2ddf853a905742c1a0773
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-18 20:42:57 +0200

    Add files via upload

---
+++ 2- Pregunta numero mayor.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+print("Dime un número:")
+A=raw_input()
+print ("Dime otro número:")
+B=raw_input()
+if A>B:
+    print "El número mayor es",A,", el primer número."
+else:
+    print "El número mayor es",B,", el segundo número."
