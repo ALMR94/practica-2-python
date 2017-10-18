1f4374e99fa54495be5f4d39156dd63086cc23ba
ALMR94 <32308491+ALMR94@users.noreply.github.com>
2017-10-18 20:45:36 +0200

    Delete 5- Media de 3 examenes.py

--- 5- Media de 3 examenes.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: cp1252 -*-
-print "Dime la nota del primer exámen:"
-a= float(raw_input())
-print "Dime la nota del segundo exámen:"
-b= float(raw_input())
-print "Dime la nota del tercer exámen:"
-c= float(raw_input())
-M= (a+b+c)/3
-print "La nota media es de",M,
